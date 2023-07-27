d66b4c1cd9b09c7dbf50c45f2219fba63619ae08
ghy <2247883756@qq.com>
2023-07-27 23:33:04 +0800

    fix confreg test

--- tests/misctest/tests/confreg-test.c
+++ tests/misctest/tests/confreg-test.c
@@ -2,6 +2,16 @@
 #include <la32r.h>
 #include <nutshell.h>
 
+int ret = 0;
+
+#define mmiocheck(cond) \
+    do { \
+      if (!(cond)) { \
+        printf("mmiocheck fail at %s:%d\n", __FILE__, __LINE__); \
+        ret = 1; \
+      } \
+    } while (0)
+
 int main()
 {
 
@@ -14,31 +24,36 @@ int main()
         uintptr_t addr = CONFREG_BASE_ADDR + 0x8000 + 0x10 * i;
         outl(addr, random);
         result = inl(addr);
-        assert(random == result);
+        mmiocheck(random == result);
     }
 
     uint32_t timer1 = inl(CONFREG_TIMER_ADDR);
     uint32_t timer2 = inl(CONFREG_TIMER_ADDR);
-    assert(timer1 != timer2);
+    mmiocheck(timer1 != timer2);
 
     outl(CONFREG_IO_SIMU_ADDR, 0x12345678);
     result = inl(CONFREG_IO_SIMU_ADDR);
-    assert(result == 0x56781234);
+    mmiocheck(result == 0x56781234);
 
     outl(CONFREG_OPEN_TRACE_ADDR, 0x80000000);
     result = inl(CONFREG_OPEN_TRACE_ADDR);
-    assert(result == 1);
+    mmiocheck(result == 1);
     outl(CONFREG_OPEN_TRACE_ADDR, 0);
     result = inl(CONFREG_OPEN_TRACE_ADDR);
-    assert(result == 0);
+    mmiocheck(result == 0);
 
     outl(CONFREG_NUM_MONITOR_ADDR, 0xffffffff);
     result = inl(CONFREG_NUM_MONITOR_ADDR);
-    assert(result == 1);
+    mmiocheck(result == 1);
     outl(CONFREG_NUM_MONITOR_ADDR, 0xfffffffe);
     result = inl(CONFREG_NUM_MONITOR_ADDR);
-    assert(result == 0);
+    mmiocheck(result == 0);
 
-    printf("confreg test pass!\n");
-    return 0;
+    if (ret) {
+      printf("confreg test fail!\n");
+    } else {
+      printf("confreg test pass!\n");
+    }
+    
+    return ret;
 }
